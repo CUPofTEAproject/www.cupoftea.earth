942679bedf0184c511b67d530e65ac12dc9e98d6
AlexisRenchon <a.renchon@gmail.com>
2021-12-14 10:08:52 -0600

    progress on FLUXNET DAMM

--- functions/DAMMfit.jl
+++ functions/DAMMfit.jl
@@ -2,9 +2,9 @@ using LsqFit
 
 include("DAMM_scaled_porosity_2.jl");
 
-lb = [0.0, 0.0, 0.0] # params can't be negative
-ub = [Inf, Inf, Inf] 
-p_ini = [1.7, 5.9, 3.15]
+lb = [0.0, 0.0, 0.0, 0.0] # params can't be negative
+ub = [Inf, Inf, Inf, Inf] 
+p_ini = [0.3, 64.0, 1.0, 1.0]
 
 function fitDAMM(Ind_var, Resp)
 	fit = curve_fit(DAMM, Ind_var, Resp, p_ini, lower=lb, upper=ub)

2a7d4c80ef293e4ca7491c7becaded8dba0fc103
AlexisRenchon <a.renchon@gmail.com>
2021-12-22 12:33:30 -0600

    colorbar and colorlimit

--- functions/FLUXNET/FNDAMMplot.jl
+++ functions/FLUXNET/FNDAMMplot.jl
@@ -1,7 +1,7 @@
 function FNDAMMplot(slider)
   fig = Figure()
   ax3D = Axis3(fig[1, 1])
-  #ax3D = LScene(fig[1, 1])
+  # ax3D = LScene(fig[1, 1])
   site_n = slider.value
   siteID = @lift(IDe[$site_n])
   outs = @lift(FNDAMMfit($siteID, 50)) 
@@ -17,9 +17,14 @@ function FNDAMMplot(slider)
   ax3D.ylabel = to_latex("\\theta (m^3 m^{-3})");
   ax3D.zlabel = to_latex("R_{soil} (\\mumol m^{-2} s^{-1})");
   data3D = @lift(Vec3f0.($Tmed, $Mmed, $Rmed))
-  p3D = scatter!(ax3D, data3D, markersize = 2500, strokewidth = 3, color = Rmed, colormap = Reverse(:Spectral))
-  s3D = surface!(ax3D, x, y, DAMM_Matrix, colormap = Reverse(:Spectral), transparency = true, alpha = 0.01, shading = false)
-  w3D = wireframe!(ax3D, x, y, DAMM_Matrix, overdraw = true, transparency = true, color = (:black, 0.1));
+  p3D = scatter!(ax3D, data3D, markersize = 2500, strokewidth = 3,
+	color = Rmed, colormap = Reverse(:Spectral), colorrange = (0, 10))
+  s3D = surface!(ax3D, x, y, DAMM_Matrix, colormap = Reverse(:Spectral),
+	transparency = true, alpha = 0.01, shading = false, colorrange = (0, 10))
+  w3D = wireframe!(ax3D, x, y, DAMM_Matrix, overdraw = true,
+	transparency = true, color = (:black, 0.1));
+  Colorbar(fig[1, 2], limits = (0, 10), colormap = Reverse(:Spectral),
+	   label = to_latex("R_{soil} (\\mumol m^{-2} s^{-1})"));
   xlims!(ax3D, -20, 50)
   ylims!(ax3D, 0, 1.0)  
   zlims!(ax3D, 0, 10)
--- functions/MAIN/FNDAMM.md
+++ functions/MAIN/FNDAMM.md
@@ -6,9 +6,18 @@ title = "DAMM Fluxnet"
 
 ```julia:ex
 #hideall
-include(joinpath("functions", "MAIN", "FNDAMM.jl"))
-#could add a line to copy the .html in /__site/FNDAMM to /menu2/, which I do manually atm
+using WGLMakie, JSServe, UnicodeFun, SparseArrays, LsqFit, DAMMmodel, DataFrames, CSV, Dates, Statistics
+# could add a line to copy the .html in /__site/FNDAMM to /menu2/, which I do manually atm
+io = IOBuffer()
+println(io, "~~~")
+show(io, MIME"text/html"(), Page(exportable=true, offline=true))
+
+include("FNDAMM.jl")
+
+show(io, MIME"text/html"(), app)
+println(io, "~~~")
+println(String(take!(io)))
 ```
 \textoutput{ex}
 
-v0.1.2
+v0.1.3

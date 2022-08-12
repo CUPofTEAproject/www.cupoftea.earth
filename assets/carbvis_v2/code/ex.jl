2267bb9bab30ecad5906068848c55f9f1fb675ff
AlexisRenchon <a.renchon@gmail.com>
2022-08-12 19:38:50 +0000

    Deploying to gh-pages from @ CUPofTEAproject/www.cupoftea.earth@4cf27444a98b946f837e3f3de2db99c9328363c6 🚀

---
+++ assets/carbvis_v2/code/ex.jl
@@ -0,0 +1,14 @@
+# This file was generated, do not modify it. # hide
+#hideall
+using WGLMakie, JSServe, CSV, DataFrames, Colors, ColorSchemes, FileIO, Downloads, GeometryBasics
+io = IOBuffer()
+println(io, "~~~")
+show(io, MIME"text/html"(), Page(exportable=true, offline=true))
+
+include("carbvis_v2.jl")
+# tes44t
+
+
+show(io, MIME"text/html"(), app)
+println(io, "~~~")
+println(String(take!(io)))
